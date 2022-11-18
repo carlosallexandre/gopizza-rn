e6a50b6ce42c16b8cfdc72be1e1fb3d2815ef68b
Carlos Alexandre <carlosallexandre@pop-os.localdomain>
2022-11-18 19:33:06 -0300

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resovler",
+        {
+          extensions: [".js", ".ts", ".tsx", ".json"],
+          root: ["./src"],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@services": "./src/services",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
